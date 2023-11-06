5f188c9332a08cb46e14a8f9a96ea62dc13e6e4d
matthew <shaggysci3@gmail.com>
2023-11-06 12:54:03 -0500

    navBar idea comment outline diliverable

---
+++ src/NavBar.jsx
@@ -0,0 +1,2 @@
+// thinking we can have all the nav links here so when we want to go somewhere we can have a slide out menu with all the navlinks on it. 
+// would be three bars left/right that when clicked pop out to a menu that has: "projects, creators, home page" 
