cf420b0db3a86b54d9aadf573a4b08866ba60d88
matthew <shaggysci3@gmail.com>
2023-11-06 14:22:59 -0500

    getting data to the project page and project card

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,12 +1,15 @@
 
-function Project({props}){
+function Project({project}){
+    
+    const {name, id, description, creators} = project
+    console.log(`project name: ${name}`)
     return (
         <>
           <div>
             <h1>project card</h1>
-            <h2>name</h2>
-            <h2>creator</h2>
-            <p>description of project</p>
+            <h2>{name}</h2>
+            <h2>{creators}</h2>
+            <p>{description}</p>
           </div>
         </>
       )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
 import Project from "./Project"
 import { NavLink } from "react-router-dom";
 
+
 function Projects({props}){
 
+
+    const [projects,setProjects] = useState([])
     //fetch the data here?
+    useEffect(() =>{
+        fetch(`https://ccserver-obi1.onrender.com/projects`)
+        .then(res => res.json())
+        .then(data => setProjects(data))
+        // .catch(error => console.error(error))
+      }, []);
+
+      const projectCards = projects.map(project=>{
+        return <Project key = {project.id} project={project}/>
+      })
+      
 
     return <>
     <div>
         <header>List of projecs go here</header>
-        <Project/>
+        {projectCards}
         <nav>
         <NavLink
         to="/"
