4c8789926fdd33b1eee4cc5e1eb2aa3c8aa0c6b8
Jack Henza <jackhenza@gmail.com>
2023-11-07 09:50:04 -0500

    make NavBar and update routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom"
-import routes from './routes.jsx'
-import './index.css'
-import { UserProvider } from './context/user.jsx'
-import NavBar from './Components/NavBar.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes.jsx"
+import "./index.css"
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <UserProvider>
-      <NavBar />
-      <RouterProvider router={router} />
-    </UserProvider>
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
