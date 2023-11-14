c8798af11819ba88cd838ef40d588f0509ba77e6
Jack Henza <jackhenza@gmail.com>
2023-11-14 11:09:28 -0500

    some fixes

--- src/Components/ProfileList.jsx
+++ src/Components/ProfileList.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import icon from '../assets/img/icon.png';
-import { useNavigate } from 'react-router-dom';
-import { useOutletContext } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import icon from "../assets/img/icon.png";
+import { useNavigate } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 
 const ProfileList = () => {
   const { user, projects, skills, creators } = useOutletContext();
@@ -11,20 +11,20 @@ const ProfileList = () => {
 
   useEffect(() => {
     // Fetch data from your JSON server
-    const dbserver = 'https://ccserver-obi1.onrender.com/';
+    const dbserver = "https://ccserver-obi1.onrender.com/";
     fetch(`${dbserver}creators`)
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         setProfiles(data);
         setLoading(false);
       })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
+      .catch(error => {
+        console.error("Error fetching data:", error);
         setLoading(false);
       });
   }, []);
 
-  const handleProfileClick = (id) => {
+  const handleProfileClick = id => {
     // Navigate to the dynamic profile route
     navigate(`/profile/${id}`);
   };
@@ -35,19 +35,36 @@ const ProfileList = () => {
         <p>Loading...</p>
       ) : (
         <div>
-          {profiles.map((profile) => (
-            <div
-              className="projectCard"
-              key={profile.id}
-              onClick={() => handleProfileClick(profile.id)}
-            >
-              <h2>{profile.name}</h2>
-              <img src={icon} alt={'profile picture'} />
-              <p>{profile.bio}</p>
-              <p>{profile.skills}</p>
-              <p>{profile.projects}</p>
-            </div>
-          ))}
+          {profiles.map(profile => {
+            let skillsList;
+            if (profile && profile.skills) {
+              skillsList = profile.skills.map(skill => {
+                return skills[skill];
+              });
+            }
+            let projectList;
+            if (profile && profile.projects) {
+              console.log(profile.projects);
+              projectList = projects.filter(project => {
+                return profile.projects.includes(project.id);
+              });
+              console.log(projectList);
+              projectList = projectList.map(project => project.name);
+            }
+            return (
+              <div
+                className="projectCard"
+                key={profile.id}
+                onClick={() => handleProfileClick(profile.id)}
+              >
+                <h2>{profile.name}</h2>
+                <img src={icon} alt={"profile picture"} />
+                <p>{profile.bio}</p>
+                <p>Skills: {skillsList.join(", ")}</p>
+                <p>Projects: {projectList.join(", ")}</p>
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
--- src/pages/ProfileList.jsx
+++ src/pages/ProfileList.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
-  const ProfileList = () => {
+const ProfileList = () => {
   const [profiles, setProfiles] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { creators, skills } = useOutletContext();
 
   useEffect(() => {
     // Fetch data from your JSON server
-    const dbserver = 'https://ccserver-obi1.onrender.com/';
+    const dbserver = "https://ccserver-obi1.onrender.com/";
     fetch(`${dbserver}creators`)
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         setProfiles(data);
         setLoading(false);
       })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
+      .catch(error => {
+        console.error("Error fetching data:", error);
         setLoading(false);
       });
   }, []);
@@ -25,15 +27,26 @@ import React, { useEffect, useState } from 'react';
         <p>Loading...</p>
       ) : (
         <div>
-          {profiles.map((profile) => (
-            <div className="projectCard" key={profile.id}>
-              <h2>{profile.name}</h2>
-              <img src={profile.img} alt={`${profile.name}'s profile picture`} />
-              <p>{profile.bio}</p>
-              <p>Skills: {profile.skills.join(', ')}</p>
-              <p>Projects: {profile.projects.join(', ')}</p>
-            </div>
-          ))}
+          {profiles.map(profile => {
+            let skillsList;
+            if (profile && profile.skills) {
+              skillsList = profile.skills.map(skill => {
+                return skills[skill];
+              });
+            }
+            return (
+              <div className="projectCard" key={profile.id}>
+                <h2>{profile.name}</h2>
+                <img
+                  src={profile.img}
+                  alt={`${profile.name}'s profile picture`}
+                />
+                <p>{profile.bio}</p>
+                <p>Skills: {skillsList.join(", ")}</p>
+                <p>Projects: {profile.projects.join(", ")}</p>
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
@@ -41,4 +54,3 @@ import React, { useEffect, useState } from 'react';
 };
 
 export default ProfileList;
-
